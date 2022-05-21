f0ebd39bd7c3e7595d676fe325d87b53093b64b9
Tetiana0310 <tetiana.posmashna@gmail.com>
2022-05-21 21:57:13 +0300

    Done task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+const intervalDelay = 1000;
+
+let timer = null;
+
+btnStart.addEventListener('click', onChangeColorBtnStart);
+btnStop.addEventListener('click', onChangeColorBtnStop);
+
+function onChangeColorBtnStart() {
+    btnStart.setAttribute('disabled')
+    timer = setInterval(onChangeColor, intervalDelay)
+}
+
+function onChangeColorBtnStop() {
+    btnStop.removeAttribute('disabled')
+    clearInterval(timer)
+}
+
+function onChangeColor() {
+     document.body.style.backgroundColor = getRandomHexColor()
+}
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
