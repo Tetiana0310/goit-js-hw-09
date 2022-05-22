510b94eceea73ae3640b949ca574d4caa8b6a21a
Tetiana0310 <tetiana.posmashna@gmail.com>
2022-05-22 20:44:41 +0300

    Done task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
+};
+
+  function onSubmit(evt) {
+  evt.preventDefault();
+    const {
+    elements: { delay, step, amount }
+  } = evt.currentTarget;
+  let valueDelay = Number(delay.value);
+  let  valueStep = Number(step.value);
+  let valueAmount = Number(amount.value);
+
+    for (let i = 1; i <= valueAmount; i += 1)
+    {createPromise(i, valueDelay)
+      .then(({position, delay}) => Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+    .catch(({position, delay}) => Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`))
+   valueDelay += valueStep;
   }
 }
+
