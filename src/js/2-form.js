31c921cf27bd4688f9f21acc129289ef8be8c7fd
Tetiana Posmashna <tetiana.posmashna@gmail.com>
2024-07-17 18:25:39 +0300

    Done task 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,43 +1,39 @@
 let formData = {
     email: "",
     message: ""
-}
+};
 
-const KEY = "feedback-form-state"
-const feedbackForm = document.querySelector(".feedback-form")
+const KEY = "feedback-form-state";
+const feedbackForm = document.querySelector(".feedback-form");
 
 feedbackForm.addEventListener('input', onInputForm);
+feedbackForm.addEventListener('submit', onFormSubmit);
 
-onSaveFormInfo()
-
-// function onSaveInfoForm() {
-//     ;
-//     if (savedData) {
-//         const {email, message} = JSON.parse(savedData);
-//         feedbackForm.email.value = formData.email;
-//         feedbackForm.message.value = formData.message;
-//         formData.email = email;
-//         formData.message = message;
-//     }
-// }
+onSaveFormInfo();
 
 function onInputForm(evt) {
-    const value = evt.target.value
-    localStorage.setItem(KEY, value)
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(KEY, JSON.stringify(formData));
 }
+
 function onSaveFormInfo() {
-    const savedData = localStorage.getItem(KEY)
-    feedbackForm.email.value = formData.email;
-        feedbackForm.message.value = formData.message;
-        formData.email = email;
-        formData.message = message; 
- }
-         
-         
-// function onFormSubmit(evt) {
-//     evt.preventDefault();
+    const savedData = localStorage.getItem(KEY);
+    if (savedData) {
+        formData = JSON.parse(savedData);
+        feedbackForm.email.value = formData.email || '';
+        feedbackForm.message.value = formData.message || '';
+    }
+}
 
-//     evt.currentTarget.reset();
-//     localStorage.removeItem(key);
+function onFormSubmit(evt) {
+    evt.preventDefault();
 
-//     console.log(formData)}
+    if (feedbackForm.email.value !== '' && feedbackForm.message.value !== '') {
+        console.log({ email: feedbackForm.email.value, message: feedbackForm.message.value });
+
+        localStorage.removeItem(KEY);
+        feedbackForm.reset();
+    } else {
+        alert('Fill please all fields');
+    }
+}
